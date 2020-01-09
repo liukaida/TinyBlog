c2ab925ffcb61f0268bf576788fe4436de6b9e81
LiuKai <liukai5431@fiberhome.com>
2020-01-09 15:07:15 +0800

    解决html无法显示的问题。

--- js/core.js
+++ js/core.js
@@ -137,22 +137,28 @@ $(document).ready(function () {
                 var base64 = new Base64();
                 var file_content = base64.decode(result.content);
 
-                testEditormdView = editormd.markdownToHTML("article", {
-                    markdown: file_content, //+ "\r\n" + $("#append-test").text(),
-                    htmlDecode: true, // 开启 HTML 标签解析，为了安全性，默认不开启
-                    htmlDecode: "style,script,iframe", // you can filter tags decode
-                    //toc             : false,
-                    tocm: true, // Using [TOCM]
-                    //tocContainer    : "#custom-toc-container", // 自定义 ToC 容器层
-                    //gfm             : false,
-                    //tocDropdown     : true,
-                    // markdownSourceCode : true, // 是否保留 Markdown 源码，即是否删除保存源码的 Textarea 标签
-                    emoji: true,
-                    taskList: true,
-                    tex: true, // 默认不解析
-                    flowChart: true, // 默认不解析
-                    sequenceDiagram: true, // 默认不解析
-                });
+                if (type == "markdown") {
+                    testEditormdView = editormd.markdownToHTML("article", {
+                        markdown: file_content, //+ "\r\n" + $("#append-test").text(),
+                        htmlDecode: true, // 开启 HTML 标签解析，为了安全性，默认不开启
+                        htmlDecode: "style,script,iframe", // you can filter tags decode
+                        //toc             : false,
+                        tocm: true, // Using [TOCM]
+                        //tocContainer    : "#custom-toc-container", // 自定义 ToC 容器层
+                        //gfm             : false,
+                        //tocDropdown     : true,
+                        // markdownSourceCode : true, // 是否保留 Markdown 源码，即是否删除保存源码的 Textarea 标签
+                        emoji: true,
+                        taskList: true,
+                        tex: true, // 默认不解析
+                        flowChart: true, // 默认不解析
+                        sequenceDiagram: true, // 默认不解析
+                    });
+                } else {
+                    // $("#title").hide();
+                    $("#article").html(file_content);
+                }
+
                 setShareUrlDom(share_path);
             });
 
@@ -222,17 +228,18 @@ function setBlogTxt(obj) {
             share_path = result.path;
             $("#copy-btn").show();  // 显示复制文章地址的按钮
             $("#title").show();
+
+            // base64转码
+            var base64 = new Base64();
+            var file_content = base64.decode(result.content);
+
             if (type == "markdown") {
 
                 $("#article").html("");
 
-                // base64转码
-                var base64 = new Base64();
-                var file_content = base64.decode(result.content);
-
                 testEditormdView = editormd.markdownToHTML("article", {
                     markdown: file_content, //+ "\r\n" + $("#append-test").text(),
-                    htmlDecode: true, // 开启 HTML 标签解析，为了安全性，默认不开启
+                    // htmlDecode: true, // 开启 HTML 标签解析，为了安全性，默认不开启
                     htmlDecode: "style,script,iframe", // you can filter tags decode
                     //toc             : false,
                     tocm: true, // Using [TOCM]
@@ -248,8 +255,8 @@ function setBlogTxt(obj) {
                 });
 
             } else {
-                $("#title").hide();
-                $("#article").html(result);
+                // $("#title").hide();
+                $("#article").html(file_content);
             }
             setShareUrlDom(share_path);
 
